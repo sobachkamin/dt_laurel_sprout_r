3ff976ef0c8a645d151a06c33b8d14da49d246c0
hemantsachdeva <hksachdeva007@gmail.com>
2020-11-21 00:41:22 +0530

    laurel_sprout:Remove packages at build time Causing https://del.dog/SYSTEMIMAGE_PARTITION_SIZE-out-of-space.txt ;-;

--- device.mk
+++ device.mk
@@ -322,6 +322,10 @@ PRODUCT_PACKAGES += \
     qti-telephony-utils \
     qti_telephony_utils.xml
 
+# Remove unwanted packages 
+PRODUCT_PACKAGES += \
+    RemovePackages
+
 # IPACM
 PRODUCT_PACKAGES += \
     ipacm \
